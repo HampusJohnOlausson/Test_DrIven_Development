826cb5dfc4e5b46ea8cac375191a3274ac4a29d1
hampus <hampus.john.olausson@gmail.com>
2021-07-09 11:32:55 +0200

    * CalculatorTests.cs:
    * Calculator.cs: Refactored the code in calculator class and testing
      successfully

--- TDD.Tests/CalculatorTests.cs
+++ TDD.Tests/CalculatorTests.cs
@@ -10,7 +10,25 @@ namespace TDD.Tests
         [InlineData("", 0)]
         [InlineData("1", 1)]
         [InlineData("1,2", 3)]
-        public void Add_AddsUpTwoNUmber_WhenStringIsValid(string calculation, int expected)
+
+        public void Add_AddsUpTwoNumber_WhenStringIsValid(string calculation, int expected)
+        {
+
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("10,90,10,20", 130)]
+        public void Add_AddsUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
         {
 
             //Arrange
--- TDD/Calculator.cs
+++ TDD/Calculator.cs
@@ -7,22 +7,11 @@ namespace TDD
     {
         public int Add(string numbers)
         {
-            var splitNumbers = numbers.Split(new[]{','}, StringSplitOptions.RemoveEmptyEntries);
+            var splitNumbers = numbers
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
 
-            if(splitNumbers.Length == 1)
-            {
-                if(!splitNumbers.Any())
-                {
-                    return 0;
-                }
-
-                if(splitNumbers.Length == 1)
-                {
-                    return int.Parse(splitNumbers[0]);
-                }
-            }
-
-            return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+            return splitNumbers.Sum();
         }
     }
 }
