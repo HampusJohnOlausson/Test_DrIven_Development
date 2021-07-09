d9b5253cd03b68aa6b4f04f7e4f4bc467902bd6e
hampus <hampus.john.olausson@gmail.com>
2021-07-09 11:16:48 +0200

    Calculator function done

---
+++ TDD/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TDD
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            var splitNumbers = numbers.Split(',');
+
+            if(!splitNumbers.Any())
+            {
+                return 0;
+            }
+
+            if(splitNumbers.Length == 1)
+            {
+                if(splitNumbers[0].Length == 0)
+                {
+                    return 0;
+                }
+
+                return int.Parse(splitNumbers[0]);
+            }
+
+            return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[2]);
+        }
+    }
+}
